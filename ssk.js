df33f3de15d5738f646edd0a242a75380a091b39
arceusss10 <saurav.jha.ug21@nsut.ac.in>
2024-05-11 23:10:06 +0530

    Add files via upload

---
+++ ssk.js
@@ -0,0 +1,21 @@
+ setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    min.style.transform = `rotate(${mrotation}deg)`;
+    sec.style.transform = `rotate(${srotation}deg)`;
+    
+}, 1000);
+
+setInterval(() => {
+    var textContainer = document.getElementById('textContainer');
+    textContainer.textContent = "text my nigga, he's waiting (,,>﹏<,,)";
+    textContainer.style.visibility = (textContainer.style.visibility == 'hidden') ? 'visible' : 'hidden';
+    
+}, 5000);
